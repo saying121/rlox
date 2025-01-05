a988d43453f624b29440eb54e95400330fbd6c2e
saying <saying121@outlook.com>
2025-01-05 17:20:47 +0800

    tests: rename

--- tests/run.rs
+++ tests/run.rs
@@ -1,14 +1,14 @@
 use test_generator::test_resources;
 
 #[test_resources("./test-resource/ok/*")]
-fn run_scripts_ok(resource: &str) {
+fn run_ok(resource: &str) {
     let lox = rlox::lox::Lox::new();
     let r = lox.run_file(resource);
     assert!(r.is_ok());
 }
 
 #[test_resources("./test-resource/err/*")]
-fn run_scripts_err(resource: &str) {
+fn run_err(resource: &str) {
     let lox = rlox::lox::Lox::new();
     let r = lox.run_file(resource);
     assert!(r.is_err());
